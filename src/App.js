e61a9d6ead0d6c4daec94b8581327b09ce24215f
gaabrielmooreira <gaabrielmooreira@gmail.com>
2023-01-26 17:40:00 -0300

    feat: Context for token implemented

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
+import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AuthContext from "./contexts/AuthContext";
 import HomePage from "./pages/HomePage";
 import SignInPage from "./pages/SignInPage";
 import SignUpPage from "./pages/SignUpPage";
 
 function App() {
+  const [token, setToken] = useState("");
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />}/>
-        <Route path="/sign-up" element={<SignUpPage />}/>
-        <Route path="/sign-in" element={<SignInPage />}/>
-      </Routes>
-    </BrowserRouter>
+    <AuthContext.Provider value={{ token, setToken }}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/sign-up" element={<SignUpPage />} />
+          <Route path="/sign-in" element={<SignInPage />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthContext.Provider>
   );
 }
 
